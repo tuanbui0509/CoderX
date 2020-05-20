f6ce1279111fd41538dfc3adee3a2e66cbef5a9a
NgocTuangit <tuanbui0509@gmail.com>
2020-05-20 10:02:55 +0700

    Add property dead cat.js

--- cat.js
+++ cat.js
@@ -1,9 +1,14 @@
 function Cat() {
     stomach = [];
+    this.dead = false;
 }
 
 Cat.prototype.eat = function(mouse){
     this.stomach.push(mouse);
+}
+
+Cat.prototype.die = function() {
+    this.dead = true;
 }
 
 module.exports = Cat;
