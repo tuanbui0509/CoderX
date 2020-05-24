eb00003256260e0ff114c8653161f5aaac3971f7
NgocTuangit <tuanbui0509@gmail.com>
2020-05-24 10:27:27 +0700

    Add console.log() for index

--- index.js
+++ index.js
@@ -7,8 +7,15 @@ var tom =  new Cat('Tom');
 var jerry = new Mouse('Jerry',1);
 
 butch.sayHi();
-tom.eat(jerry);
-butch.eat(tom);
+console.log("Before");
 console.log(jerry);
 console.log(tom);
+tom.eat(jerry);
+jerry.die();
+butch.eat(tom);
+tom.die();
+console.log(tom);
 console.log(butch);
+console.log("After being ate tom and jerry are died");
+console.log(jerry);
+console.log(tom);
