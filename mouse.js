d35cc180b1397d114e7e82d2b5db002b8c0f2c60
NgocTuangit <tuanbui0509@gmail.com>
2020-06-01 14:22:20 +0700

    Add method sleep for mouse

--- mouse.js
+++ mouse.js
@@ -12,4 +12,8 @@ Mouse.prototype.run = function(){
     console.log(`A mouse is running`);
 }
 
+Mouse.prototype.sleep = function(){
+    console.log(`A mouse is sleeping`);
+}
+
 module.exports = Mouse;
